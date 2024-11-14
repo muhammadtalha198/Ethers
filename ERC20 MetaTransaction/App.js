e476663071710d6fd9a88b523b44e0a53938654c
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2024-11-14 23:23:27 +0500

    Create App.js

---
+++ ERC20 MetaTransaction/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import PermitComponent from "./PermitComponent";
+import MetaTransactionComponent from "./MetaTransactionComponent";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <PermitComponent /> */}
+      <MetaTransactionComponent />
+    </div>
+  );
+}
+
+export default App;
